232a69bcc28355f535746e117e4047f3c94ec46f
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-03-22 23:08:30 +0900

    Add Image

--- ZeusMethod/Welcome/WelcomeView.swift
+++ ZeusMethod/Welcome/WelcomeView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.welcomeBackground)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        }
     }
 }
 
