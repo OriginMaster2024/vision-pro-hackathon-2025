61e92a1e8cdb29cdd17f94b7eaf01ca8c2acedb2
Hatsune Mineta <hatsune.mineta@smartnews.com>
2025-03-22 13:25:22 +0900

    add shooter

---
+++ ZeusMethod/Shooter.swift
@@ -0,0 +1,21 @@
+//
+//  Shooter.swift
+//  ZeusMethod
+//
+//  Created by Hatsune Mineta on 2025/03/22.
+//
+
+import RealityKit
+
+class Shooter {
+    private static let SHOOT_DURATION = 0.3
+    
+    static func shoot(entity: Entity, to targetPosition: SIMD3<Float>) {
+        entity.move(
+            to: Transform(translation: targetPosition),
+            relativeTo: nil,
+            duration: SHOOT_DURATION,
+            timingFunction: .easeInOut
+        )
+    }
+}
