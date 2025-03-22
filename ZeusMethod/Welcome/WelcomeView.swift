000913d7efba0912dd0b4e2957d18d59d4894f50
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-03-22 23:04:09 +0900

    Add WelcomeView

---
+++ ZeusMethod/Welcome/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  ZeusMethod
+//
+//  Created by Akihiro Kokubo on 2025/03/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
