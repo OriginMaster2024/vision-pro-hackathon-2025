fa380bfd7c75886f2567162bdfccb47ee6901df0
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-03-22 23:10:11 +0900

    Add text

--- ZeusMethod/Welcome/WelcomeView.swift
+++ ZeusMethod/Welcome/WelcomeView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         ZStack {
             Image(.welcomeBackground)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
+            VStack {
+                Text("天空道場")
+                    .font(.system(size: 80))
+                Text("拳で覚える星座ドリル")
+                    .font(.system(size: 40))
+            }
         }
     }
 }
