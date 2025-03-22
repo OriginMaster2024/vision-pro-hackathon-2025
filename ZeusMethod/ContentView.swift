d9c90db470a5ea5f75e2a2acd66638c42ab32c21
Hatsune Mineta <hatsune.mineta@smartnews.com>
2025-03-22 14:13:13 +0900

    generate and shoot multiple stars

--- ZeusMethod/ContentView.swift
+++ ZeusMethod/ContentView.swift
@@ -7,12 +7,29 @@
 
 import SwiftUI
 import RealityKit
+import RealityKitContent
 
 struct ContentView: View {
+    @Environment(AppModel.self) var appModel
 
     var body: some View {
         VStack {
             ToggleImmersiveSpaceButton()
+            Button("Start!") {
+                Task {
+                    await generateStars(count: 5);
+                }
+            }
+        }
+    }
+    
+    private func generateStars(count: Int) async {
+        for i in 0..<count {
+            if let sphere = try? await Entity(named: "Sphere", in: realityKitContentBundle) {
+                sphere.position = [0, 1, -1]
+                appModel.spheres.append(sphere)
+                print("Generated \(i+1)th star.")
+            }
         }
     }
 }
