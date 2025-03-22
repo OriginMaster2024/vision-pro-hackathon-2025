443153b2d635a3e56d8c0eede99327518fc85674
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-03-23 07:36:17 +0900

    Add center and targetCenter

--- ZeusMethod/MoveConstellationView.swift
+++ ZeusMethod/MoveConstellationView.swift
@@ -12,6 +12,13 @@ struct MoveConstellationView: View {
         .init(x: 2, y: 2, z: 0),
         .init(x: 2, y: 1, z: 0),
     ]
+    var center: SIMD3<Float> {
+        let sum = positions
+            .map { $0.simd3 }
+            .reduce(SIMD3<Float>(repeating: 0), +)
+        return sum / Float(positions.count)
+    }
+    let targetCenter: Position = .init(x: 0, y: 5, z: -10)
     
     var body: some View {
         ZStack {
@@ -19,6 +26,11 @@ struct MoveConstellationView: View {
                 GlowingSphereView(position: position.simd3)
                     .frame(depth: 0)
             }
+            SphereView(position: center, radius: 0.1)
+                .frame(depth: 0)
+            
+            SphereView(position: targetCenter.simd3, radius: 0.1)
+                .frame(depth: 0)
         }
     }
 }
