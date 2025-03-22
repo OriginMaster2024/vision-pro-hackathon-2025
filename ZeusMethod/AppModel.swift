191ef208be8a1552269169d6c778be22a7bf9250
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-03-22 15:48:52 +0900

    Add properties of stars and lines

--- ZeusMethod/AppModel.swift
+++ ZeusMethod/AppModel.swift
@@ -27,4 +27,47 @@ class AppModel {
     
     // 加速度計算のための直前2回のposition
     var prevPositions: [SIMD3<Float>] = []
+    
+    
+    /// 正しい星の位置情報のリスト
+    let correctStarPositions: [Position] = [
+        .init(x: -2, y: 8, z: -10),
+        .init(x: 1, y: 8, z: -10),
+        .init(x: 0, y: 6, z: -10),
+        .init(x: 2, y: 4, z: -10),
+        .init(x: 1, y: 2, z: -10),
+    ]
+    /// 星のノード間に引く線分のリスト
+    /// indexで指定する
+    let indexedLines: [IndexedLine] = [
+        .init(headIndex: 0, tailIndex: 1),
+        .init(headIndex: 1, tailIndex: 2),
+        .init(headIndex: 2, tailIndex: 3),
+        .init(headIndex: 3, tailIndex: 4),
+    ]
+    
+    /// 正しい星座の線分のリスト
+    var correctLines: [LineSegment] {
+        indexedLines.compactMap { indexedLine in
+            guard let head = correctStarPositions.get(indexedLine.headIndex),
+                  let tail = correctStarPositions.get(indexedLine.tailIndex)
+            else {
+                return nil
+            }
+            return LineSegment(head: head.simd3, tail: tail.simd3)
+        }
+    }
+    /// パンチによって指定された星の位置情報のリスト
+    var starPositions: [Position] = []
+    /// パンチによる星座の線分のリスト
+    var lines: [LineSegment] {
+        indexedLines.compactMap { indexedLine in
+            guard let head = starPositions.get(indexedLine.headIndex),
+                  let tail = starPositions.get(indexedLine.tailIndex)
+            else {
+                return nil
+            }
+            return LineSegment(head: head.simd3, tail: tail.simd3)
+        }
+    }
 }
