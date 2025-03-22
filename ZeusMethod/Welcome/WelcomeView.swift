e8e10f1dc34318b2b66b6750ecff401de68f5e62
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-03-22 23:20:53 +0900

    fix spacing

--- ZeusMethod/Welcome/WelcomeView.swift
+++ ZeusMethod/Welcome/WelcomeView.swift
@@ -13,20 +13,22 @@ struct WelcomeView: View {
             Image(.welcomeBackground)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-            VStack(spacing: 32) {
-                Text("天空道場")
-                    .font(.system(size: 80))
-                Text("拳で覚える星座ドリル")
-                    .font(.system(size: 40))
+            VStack(spacing: 88) {
+                VStack(spacing: 32) {
+                    Text("天空道場")
+                        .font(.system(size: 80))
+                    Text("拳で覚える星座ドリル")
+                        .font(.system(size: 40))
+                }
                 
                 Button(action: {
                     print("Enter Immersive Space")
                 }, label: {
                     Text("門を叩く")
                         .font(.system(size: 32))
+                        .foregroundStyle(.primary)
                         .frame(width: 320, height: 64)
                         .clipShape(RoundedRectangle(cornerRadius: 32))
-                        .background(.primary)
                 })
             }
         }
